506dc25eabfc13716ef9fdf0eb4825e60b6fe23c
web3pavlou <web3pavlou@hotmail.com>
2025-06-06 14:00:13 +0300

    refactor: use explicit import syntax for AggregatorV3Interface

--- test/mocks/MockV3Aggregator.sol
+++ test/mocks/MockV3Aggregator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "chainlink/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title MockV3Aggregator
